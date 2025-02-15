50a00e09a88534e704746d2c1fe17019a7ed0b6c
Nick Xenias <nick.xenias@gmail.com>
2025-02-15 14:06:18 +0000

    Mod: additional documentation and api changes to polyphase.h

--- include/polyphase.h
+++ include/polyphase.h
@@ -16,12 +16,12 @@
 //==============================================================================
 //  Name:     polyphase.h
 //
-//  Purpose:  Generates a polyphase filter bank from an FIR input filter
+//  Purpose:  Generates a polyphase filter bank from a FIR input filter
 //
 //  Created:  2022/11/05
 // 
 //  Description:
-//    Generates a polyphase filter bank from an FIR input filter.  For various
+//    Generates a polyphase filter bank from a FIR input filter.  For various
 //    classes of windowed sinc filters, this class can generate the canonical
 //    filter then divide it into a filter bank.  All filters in the polyphase
 //    filter bank will be aligned to start on an address boundary equal to the
@@ -61,13 +61,18 @@
 #include <math.h>
 #include <string>
 #include <stdexcept>
-#include "complex_num.h"  // for Complex8 data type
-#include "evm.h" // for numerical math constant pi_d
+#include <complex>  // for Complex8 data type
+#include "evm.h"    // for numerical math constant pi_d
 
 using std::vector;
 using std::string;
 using std::cerr;
 using std::endl;
+using std::complex;
+
+#ifndef Complex8
+#define Complex8 complex<float>
+#endif
 
 class Polyphase
 {
@@ -75,24 +80,56 @@ class Polyphase
     //==============//
     // Constructors //
     //==============//
-    // Default constructor creates a small windowed sinc filter bank
-    inline Polyphase();
+    // Default constructor creates a small windowed sinc filter bank, the total
+    // number of taps in the canonical (high rate) windowed sinc filter will be
+    // tapsPerFilt*numFilters but may contain some 0-valued taps on the edges to
+    // ensure the filter is centered on time T=0 and that one of the taps is at
+    // time T=0
+    //    numFilters = desired number of filters in the polyphase bank, 
+    //    tapsPerFilt = number of teps per filter in each low rate filter in
+    //        the bank
+    //    bw = bandwidth of the sinc interpolation filter as a fraction of
+    //        the input sampling rate, i.e. at the rate of the polyphase filter
+    //        not the upsampled canonical filter (a bw of .95 means 95% of the
+    //        input bw, a bw > 1 is allowed but may result in aliasing)
+    //    win = window function for the sinc interpolation filter, see
+    //        genfilter.h for choices as this class is actually used to
+    //        generate the canonical filter
+    inline Polyphase(const int numFilters=3, const int tapsPerFilt=3,
+        const double bw=1.0, const GenFilter::WinType win=GenFilter::HANN);
 
-    // Constructor for generating a filter bank from a user defined filter,
-    // the length of the filter is assumed to be numFilters*numTaps.
-    //    filter = canonical filter taps to generate bank from, assumed to be
-    //        length numFilters*tapsPerFilt
-    //    isCX = whether taps are real or complex format
+    // Constructor for generating a SRRC filter bank, the total number of taps
+    // in the canonical (high rate) SRRC filter will be tapsPerFilt*numFilters
+    // but may contain some 0-valued taps on the edges to ensure the filter is
+    // centered on time T=0 and that one of the taps is at T=0
+    //    numFilters = desired number of filters in the polyphase bank, 
+    //    tapsPerFilt = number of teps per filter in each low rate filter in
+    //        the bank
+    //    bw = bandwidth of the sinc interpolation filter as a fraction of
+    //        the input sampling rate, i.e. at the rate of the polyphase filter
+    //        not the upsampled canonical filter (a bw of .95 means 95% of the
+    //        input bw, a bw > 1 is allowed but may result in aliasing)
+    //    rolloff = rollof factor for SRRC pulse shape, must be between 0 and 1
+    //        inclusive, a rolloff of 0 is a perfect sinc, a rolloff of 1 has
+    //        100% excess bw from rolloff 0
+    inline Polyphase(const int numFilters, const int tapsPerFilt,
+        const double bw, const double rolloff);
+
+    // Constructor for generating a filter bank from a user defined canonical
+    // filter
+    //    filter = canonical filter taps to generate bank from
+    //    ntaps = number of taps in the filter
+    //    isCX = whether taps are real or complex valued (both are assumed to
+    //        be single precision floating point)
     //    numFilters = number of filters to split the canonical filter into
-    //    tapsPerFilt = number of taps per split filter (i.e. polyphase leg)
     //    centerTap = index of the center or time t=0 tap, may be fractional
     //        if the specific tap corresponding to t=0 (i.e. phase of 0) is
     //        not in the filter, must be >= 0 and < numFilters*tapsPerFilt
-    //    firstPhase = interpolation phase of the first filter in the bank,
-    //        filters will be ordered in increasing phase increments of
+    //    firstPhase = desired interpolation phase of the first filter in the
+    //        bank, filters will be ordered in increasing phase increments of
     //	      1.0/numFilters cycles
-    inline Polyphase(const char *filter, const bool isCX, const int numFilters,
-        const int tapsPerFilt, const double centerTap, const double firstPhase);
+    inline Polyphase(char const * const filter, const int ntaps, const bool isCX,
+        const int numFilters, const double centerTap, const double firstPhase);
 
     // Constructor for generating a windowed sinc filter bank
 
